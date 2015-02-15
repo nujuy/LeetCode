689bb6b7eb4388e577a728a075ebbdc36b1dfed9
Yujun Chen <chenyujun9034@hotmail.com>
2015-02-14 23:59:21 -0500

    Update #3 LongestSubstringWithoutRepeatingCharacters.java
    如果本题难一点，试考虑：即返回最大值还要返回最大值的具体的string。
    -- 一个思路是将所有子字符串作比较，复杂度是O(n^2). 
    本题只要求返回长度，所以使用了HashSet和Array的index来解。复杂度是O(n).

--- #3 LongestSubstringWithoutRepeatingCharacters.java
+++ #3 LongestSubstringWithoutRepeatingCharacters.java
@@ -1,1 +1,27 @@
-public 
+public  int lengthOfLongestSubstring(String s){
+  if(s==null && s.length == 0){
+    return 0; 
+  }
+  
+  HashSet<Character> set = new HashSet<Character>(); //set is used to hold the longest string
+  
+  int max = 0;
+  int head = 0;
+  int tail = 0;
+  
+  while(head < s.length()){
+    if(!set.contains(s.charat(head)){
+      set.add(s.charAt(head));
+    }
+    head++;
+    
+    else{
+      if (max < head - tail){
+        max = head - tail;
+      }
+      
+    }
+  }
+  
+  
+}
