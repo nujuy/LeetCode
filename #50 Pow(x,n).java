7da0ac4afc8b9dffad9bf6cd4cffe365738c318d
Yujun Chen <chenyujun9034@hotmail.com>
2015-02-17 19:28:29 -0500

    Update #50 Pow(x,n).java

--- #50 Pow(x,n).java
+++ #50 Pow(x,n).java
@@ -1,5 +1,22 @@
 public class Solution {
-    public double pow(double x, int n) {
-        
-    }
+    public double power(double x, int n) {
+	if (n == 0)
+		return 1;
+ 
+	double v = power(x, n / 2);
+ 
+	if (n % 2 == 0) {
+		return v * v;
+	} else {
+		return v * v * x;
+	}
+}
+ 
+public double pow(double x, int n) {
+	if (n < 0) {
+		return 1 / power(x, -n);
+	} else {
+		return power(x, n);
+	}
+}
 }
